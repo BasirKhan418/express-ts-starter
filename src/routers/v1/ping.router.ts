76c181062b3c8416c63440f018009207070e4437
Basirkhan418 <thebasirkhanofficial@gmail.com>
2025-07-24 23:38:59 +0530

    zod layer added

--- src/routers/v1/ping.router.ts
+++ src/routers/v1/ping.router.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 const Pingrouter = express.Router();
 import PingController from '../../controllers/ping.controller';
-Pingrouter.get('/',PingController );
+import { ValidateRequestBody } from '../../validators';
+import { PingSchema } from '../../validators/ping.validator';
+Pingrouter.get('/', ValidateRequestBody(PingSchema), PingController );
 export default Pingrouter;
